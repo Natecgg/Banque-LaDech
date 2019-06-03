b1b1ce3aca7afb4fd24f2a87a74496e139e87da4
AlexandreGross <gross.alexandre@hotmail.fr>
2019-06-03 16:33:44 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	src/fr/fatorfit/ihm/FnInscription.form
    	src/fr/fatorfit/ihm/FnInscription.java

--- src/java/fr/dev13/servlet/EspaceClientServlet.java
+++ src/java/fr/dev13/servlet/EspaceClientServlet.java
@@ -17,8 +17,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ESIC
  */
-@WebServlet(name = "MenuServlet", urlPatterns = {"/MenuServlet"})
-public class MenuServlet extends HttpServlet {
+@WebServlet(name = "EspaceClientServlet", urlPatterns = {"/espace_client"})
+public class EspaceClientServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,10 +37,10 @@ public class MenuServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet MenuServlet</title>");            
+            out.println("<title>Servlet EspaceClientServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet MenuServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet EspaceClientServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
